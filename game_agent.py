693487f970dc3584ea1b273d51e4d0a1843520a5
yaj <yajneshvar.arumugam@gmail.com>
2017-06-11 16:03:34 -0400

    Final heuristics functions

--- game_agent.py
+++ game_agent.py
@@ -3,6 +3,7 @@ test your agent's strength against a set of known agents using tournament.py
 and include the results in your report.
 """
 import random
+import math
 
 
 class SearchTimeout(Exception):
@@ -11,8 +12,8 @@ class SearchTimeout(Exception):
 
 
 def custom_score(game, player):
-    """Calculate the heuristic value of a game state from the point of view
-    of the given player.
+    """Quantifies a player move where a move in the center of square is weighted higher
+    while the center space is not crowded
 
     This should be the best heuristic function for your project submission.
 
@@ -34,67 +35,168 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    if game.is_winner(player):
+        return float("+inf")
+    elif game.is_loser(player):
+        return float("-inf")
 
-    return float(len(game.get_legal_moves(player)))
-
-
-def custom_score_2(game, player):
-    """Calculate the heuristic value of a game state from the point of view
-    of the given player.
-
-    Note: this function should be called from within a Player instance as
-    `self.score()` -- you should not need to call this function directly.
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : object
-        A player instance in the current game (i.e., an object corresponding to
-        one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-    Returns
-    -------
-    float
-        The heuristic value of the current game state to the specified player.
-    """
-
-    moves = game.get_legal_moves(player)
-    return calculate_weigthed_score(moves, game.height, game.width)
-
-
-def custom_score_3(game, player):
-    """Calculate the heuristic value of a game state from the point of view
-    of the given player.
-
-    Note: this function should be called from within a Player instance as
-    `self.score()` -- you should not need to call this function directly.
-
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : object
-        A player instance in the current game (i.e., an object corresponding to
-        one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-    Returns
-    -------
-    float
-        The heuristic value of the current game state to the specified player.
-    """
     opponent = game.get_opponent(player)
     opponent_moves = game.get_legal_moves(opponent)
     my_moves = game.get_legal_moves(player)
     my_weighted_score = calculate_weigthed_score(my_moves, game.height, game.width)
     opponent_weighted_score = calculate_weigthed_score(opponent_moves, game.height, game.width)
 
-    return my_weighted_score - opponent_weighted_score
+    open_moves = len(game.get_blank_spaces())
+    total_space = game.height*game.width
+    if center_space_ratio(game) > 0.6:
+        return my_weighted_score - opponent_weighted_score
+    else:
+        return float(len(my_moves) - len(opponent_moves))
+
+
+def custom_score_2(game, player):
+    """ while the center of board is empty game is scored on the distance between players
+        when the center of board becomes crowded evaluate the number of moves available
+
+    Note: this function should be called from within a Player instance as
+    `self.score()` -- you should not need to call this function directly.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+
+    Returns
+    -------
+    float
+        The heuristic value of the current game state to the specified player.
+    """
+    if game.is_winner(player):
+        return float("+inf")
+    elif game.is_loser(player):
+        return float("-inf")
+
+    moves = game.get_legal_moves(player)
+    opponent_moves = game.get_legal_moves(game.get_opponent(player))
+
+    if center_space_ratio(game) > 0.6:
+        return distance_between_player(game)
+    return float(len(moves) - len(opponent_moves))
+
+
+def custom_score_3(game, player):
+    """Calculates a weighted sum of the possible moves for the player
+        When the board is less crowded it assigns more weight to moves far from the board
+        When the board is crowded assigns more priority to greater number of moves
+
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+
+    Returns
+    -------
+    float
+        The heuristic value of the current game state to the specified player.
+    """
+    if game.is_winner(player):
+        return float("+inf")
+    elif game.is_loser(player):
+        return float("-inf")
+
+    free_ratio = free_space_ratio(game)
+    opponent_moves = game.get_legal_moves(game.get_opponent(player))
+    my_moves = game.get_legal_moves(player)
+    return free_ratio*(distance_from_center(game, player)) + (1-free_ratio)*(float(len(my_moves) - len(opponent_moves)))
+
+
+def free_space_ratio(game):
+    """ Calculates the ratio of blank space to board spaces
+
+    Parameters
+    ----------
+    game: `isolation.Board`
+        An instance of `isolation.Board`
+
+    Returns
+    -------
+    float
+        Ratio of available spaces
+    """
+    free_space_count = len(game.get_blank_spaces())
+    total_space = game.height * game.width
+    return free_space_count/total_space
+
+
+def center_space_ratio(game):
+    """ Calculates the ratio of center space to board spaces
+
+    Parameters
+    ----------
+    game: `isolation.Board`
+        An instance of `isolation.Board`
+
+    Returns
+    -------
+    float
+        Ratio of available center spaces
+    """
+    def center_filter(move, h, w):
+        return 0 + 2 < move[0] < w - 2 and 0 + 2 < move[1] < h - 2
+    center_spaces = list(filter(lambda mov: center_filter(mov,game.height, game.width), game.get_blank_spaces()))
+    return float(len(center_spaces)/(game.width*game.height))
+
+
+def distance_between_player(game):
+    """ Calculates the distance between players
+
+    Parameters
+    ----------
+    game: `isolation.Board`
+        An instance of `isolation.Board`
+
+    Returns
+    -------
+    float
+        distance between players
+    """
+    loc = game.get_player_location(game.active_player)
+    loc_oppponent = game.get_player_location(game.inactive_player)
+    distance_between = math.sqrt((loc[0] - loc_oppponent[0])**2 + (loc[1] - loc_oppponent[1])**2)
+    return distance_between
+
+
+def distance_from_center(game, player):
+    """ Calculates the distance between player and center of board
+
+    Parameters
+    ----------
+    game: `isolation.Board`
+        An instance of `isolation.Board`
+
+    player : object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+
+    Returns
+    -------
+    float
+        distance between player and center of board
+    """
+    w, h = game.width / 2., game.height / 2.
+    y, x = game.get_player_location(player)
+    return float((h - y)**2 + (w - x)**2)
 
 
 def calculate_weigthed_score(list_moves, height, width):
@@ -117,8 +219,31 @@ def calculate_weigthed_score(list_moves, height, width):
 
 
 def assign_weight(move, h, w):
+    """
+    Weighted score of possible moves
+    Parameters
+    ----------
+    move: (integer, integer)
+        Possible move in a board (x,y)
+    h: integer
+        height of board
+    w: integer
+        width of board
+
+    Returns
+    -------
+    float
+        Weighted value of a move
+    """
+
+    x_cor = [0,w-1]
+    y_cor = [0,h-1]
+
+    corner_pieces = [(x,y) for x in x_cor for y in y_cor]
     if 0 + 2 < move[0] < w - 2 and 0 + 2 < move[1] < h - 2:
         return 2.0
+    elif move in corner_pieces:
+        return 0.5
     return 1.0
 
 
@@ -245,6 +370,9 @@ class MinimaxPlayer(IsolationPlayer):
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
         moves = game.get_legal_moves()
+        if len(moves) == 0:
+            return -1, -1
+
         result = max([(self.minvalue(game.forecast_move(move), game.active_player, depth-1), move)for move in moves],key=lambda x: x[0])
         return result[1]
 
